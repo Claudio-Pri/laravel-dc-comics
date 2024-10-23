b1d484ee404f520be538a20fc2af5cb5de8a8e7e
Claudio-Pri <priv.claudio@gmail.com>
2024-10-23 17:07:32 +0200

    ComicSeeder

--- database/seeders/ComicSeeder.php
+++ database/seeders/ComicSeeder.php
@@ -5,6 +5,9 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+// Models
+use App\Models\Comic;
+
 class ComicSeeder extends Seeder
 {
     /**
@@ -12,6 +15,36 @@ class ComicSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Comic::truncate();
+        // leggo il file comics.php e creo una nuova istanza di comic
+        $comics = config('comics');
+        // dd($comics);
+        foreach ($comics as $singleComic) {
+            $comic = new Comic();
+
+            $comic->title = $singleComic['title'];
+            $comic->description = $singleComic['description'];
+            $comic->thumb = $singleComic['thumb'];
+
+            //prendo il prezzo senza il simbolo di valuta
+            $priceNumber = floatval(substr($singleComic['price'], 1));
+            //uso il numero
+            $comic->price = $priceNumber;
+
+            $comic->series = $singleComic['series'];
+            $comic->sale_date = $singleComic['sale_date'];
+            $comic->type = $singleComic['type'];
+
+            //codifico l'array
+            $jsonArtists = json_encode($singleComic['artists']);
+            //salvo l'array trasformato in stringa json
+            $comic->artists = $jsonArtists;
+
+            $jsonWriters = json_encode($singleComic['writers']);
+            $comic->writers = $jsonWriters;
+
+            $comic->save();
+        }
+
     }
 }
